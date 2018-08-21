228cc162748482234541abdbc82506a5c68b1920
Zuza Zuber <zuberzuzanna@gmail.com>
2018-08-21 18:53:25 +0200

    fix search dates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,14 +10,21 @@ class EventsController < ApplicationController
       @events = policy_scope(Event).search_address(params[:query])
       if params[:date_from].present?
         d = params[:date_from].split("-").map {|x| x.to_i}
-
         @events = @events.select{ |event| event.date >= Date.new(d[0], d[1], d[2]) }
+      end
+      if params[:date_to].present?
+        d = params[:date_to].split("-").map {|x| x.to_i}
+        @events = @events.select{ |event| event.date <= Date.new(d[0], d[1], d[2]) }
       end
     else
       @events = policy_scope(Event)
       if params[:date_from].present?
         d = params[:date_from].split("-").map {|x| x.to_i}
         @events = @events.select{ |event| event.date >= Date.new(d[0], d[1], d[2])  }
+      end
+      if params[:date_to].present?
+        dt = params[:date_to].split("-").map {|x| x.to_i}
+        @events = @events.select{ |event| event.date <= Date.new(dt[0], dt[1], dt[2]) }
       end
     end
 
