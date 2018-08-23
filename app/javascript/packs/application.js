29b34ff80420a5e2a854d8ad9bdef7231f7daba2
Zuza Zuber <zuberzuzanna@gmail.com>
2018-08-23 15:37:47 +0200

    update js for arrows

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,32 +17,41 @@ global.jQuery = jQuery;
 // })};
 // }
 
-document.getElementById("arrow-1").addEventListener("click", event => {
-  document.getElementById("sport-1").scrollBy(300,0);
-});
+let arrow = document.getElementById("arrow-1");
 
-let arrow = document.getElementById("arrow-2");
+if (arrow) { arrow.addEventListener("click", event => {
+  document.getElementById("sport-1").scrollBy(300,0);
+})};
+
+arrow = document.getElementById("arrow-2");
 
 if (arrow) { arrow.addEventListener("click", event => {
   document.getElementById("sport-2").scrollBy(300,0);
 })};
 
-document.getElementById("arrow-3").addEventListener("click", event => {
+arrow = document.getElementById("arrow-3");
+
+if (arrow) { arrow.addEventListener("click", event => {
   document.getElementById("sport-3").scrollBy(300,0);
-});
+})};
 
-document.getElementById("arrow-4").addEventListener("click", event => {
+arrow = document.getElementById("arrow-4");
+
+if (arrow) { arrow.addEventListener("click", event => {
   document.getElementById("sport-4").scrollBy(300,0);
-});
+})};
 
-document.getElementById("arrow-5").addEventListener("click", event => {
+arrow = document.getElementById("arrow-5");
+
+if (arrow) { arrow.addEventListener("click", event => {
   document.getElementById("sport-5").scrollBy(300,0);
-});
+})};
 
-document.getElementById("arrow-6").addEventListener("click", event => {
+arrow = document.getElementById("arrow-6");
+
+if (arrow) { arrow.addEventListener("click", event => {
   document.getElementById("sport-6").scrollBy(300,0);
-});
-
+})};
 
 
 document.querySelector("#free").addEventListener("click", event => {
