1d7516e0a1b00b73d54b2bce263f114709a5f093
Zuza Zuber <zuberzuzanna@gmail.com>
2018-08-29 18:54:23 +0200

    update sign up form and remove scroll down with tabs

--- app/javascript/components/toggle.js
+++ app/javascript/components/toggle.js
@@ -2,9 +2,11 @@ let button = document.querySelector('.slider')
 let label = document.querySelector('.switch-label')
 
 
-function toggle() {button.addEventListener('click', event => {
+function toggle() {
+  if (button && toggle) {
+  button.addEventListener('click', event => {
   console.log('hello');
   label.classList.toggle('orange');
-})};
+})}};
 
 export { toggle };
